e5b297363837315d0dd96f1bdabc652d21233113
Sangwon <born9507@snu.ac.kr>
2020-08-30 16:49:09 +0900

    Add NEW2.py, Add recognition.py

--- src/model.py
+++ src/model.py
@@ -16,7 +16,7 @@ def model_basic():
     model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
-
+    
     model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
