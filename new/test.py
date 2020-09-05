b72f621c0db8d3e496a72d07c6232a0abfcd58b4
sangwon <born9507@snu.ac.kr>
2020-09-05 08:24:57 +0100

    Add face_location code to view func

--- new/test.py
+++ new/test.py
@@ -5,142 +5,124 @@ import numpy as np
 import time
 import cv2
 import json
+import asyncio
 import face_recognition
 
-# model
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Dropout, Flatten
-
+# from model.model import model
 #recognition
-from keras.models import load_model
+# from keras.models import load_model
 
 # view 함수는 카메라로 촬영한 것을 frame 변수에 저장(np.array, dtype=float64)
-def view(frame, is_running, ):
+# 얼굴 위치도 계산해서 내보냄(2명 이상이면 한명만 골라서)
+
+def view(frame, HEIGHT, WIDTH, face_location, is_running, ):
+
     capture = cv2.VideoCapture(-1)
+    capture.set(3, WIDTH.value)
+    capture.set(4, HEIGHT.value)
+
+    face_cascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_alt2.xml')
+    # info = ''
+    # font = cv2.FONT_HERSHEY_SIMPLEX
+
     while True:
+        start = time.time()
         if is_running.value==1:
             ret, frame_ = capture.read()
             if not ret: break
-            frame.array[:] = frame_[:]
-            # 중요! 값을 하나하나 넣어준다. 통채로 대입하면 메모리 주소가 달라지는 듯
+
+            gray = cv2.cvtColor(frame_, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            # cv2.putText(frame_, info, (5, 15), font, 0.5, (255, 0, 255), 1)
+            
+            frame.array[:] = frame_[:] # export
+
+            if len(faces)>1:
+                face_list = []
+                for face in faces:
+                    face_list.append(face[2])
+                faces = np.array([faces[np.argmax(np.array(face_list))]])
+
+            if len(faces)==1:
+                if is_detected.value == 0:
+                    is_detected.value = 1 
+                [x, y, w, h] = faces[0]
+                face_locations = np.array([[y, x+w, y+h, x]])
+                face_location.array[:] = face_locations[:] # export
+                print("detected!")
+                # (top, right, bottom, left) = (y, x+w, y+h, x)
+                # cv2.rectangle(frame_, (left, top), (right, bottom), (0,0,255), 2)
+            # No face detected
+            else:
+                if is_detected.value == 1:
+                    is_detected.value = 0
+                else:
+                    pass
+            # frame = cv2.flip(frame, 1)
+            # cv2.imshow("frame", frame_)
+            # if cv2.waitKey(1) == ord('q'): break
         else:
             time.sleep(1)
             pass
+        print("view time: ", time.time()-start)
 
-######################################################################
+    capture.release() 
+    cv2.destroyAllWindows()
 
-# face_tracking 함수는 얼굴 위치를 계산해서 내보냄(2명 이상이면 한명만 골라서)
-# 입력: frame, is_running  출력: face_location, is_detected
-def face_location(frame, face_location, is_running, is_detected, ):
-    face_cascade = cv2.CascadeClassifier('haar/haarcascade_frontalface_alt2.xml')
+
+def face_tracking(face_location, is_running, ):
     while True:
-        uint8 = frame.array.astype(np.uint8)
-        gray = cv2.cvtColor(uint8, cv2.COLOR_RGB2GRAY)
-        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
-        # --------------------추가된 부분--------------------
-        # 얼굴 회전할 각도 설정하는 array, 테스트 결과 +-15도까지는 haar cascade filter가 동작함
-        # 각각의 각도만큼 회전된 이미지를 바탕으로 얼굴 위치 검출, faces array에 추가
-        
-        # rotationAngleArray = [-30, 0, 30]
-        # faces = []
-        # (height, width) = gray.shape[:2]
-        # imageCenter = (width / 2, height / 2)
-
-        # for angle in rotationAngleArray:
-        #     rotMatrix = cv2.getRotationMatrix2D(imageCenter, angle, 1)
-        #     Cos = rotMatrix[0, 0]
-        #     Sin = rotMatrix[0, 1]
-        #     rotatedWidth = int(height * abs(Sin) + width * abs(Cos))
-        #     rotatedHeight = int(height * abs(Cos) + width * abs(Sin))
-        #     rotMatrix[0, 2] += rotatedWidth / 2 - imageCenter[0]
-        #     rotMatrix[1, 2] += rotatedHeight / 2 - imageCenter[1]
-        #     targetFrame = cv2.warpAffine(gray, rotMatrix, (rotatedWidth, rotatedHeight))
-
-        #     faceLocations = face_cascade.detectMultiScale(gray, 1.3, 5)
-            
-        #     if faceLocations != ():
-        #         for location in faceLocations:
-        #             (x, y, w, h) = location
-        #             midx = x + w/2
-        #             midy = y + h/2
-        #             x = int(Cos * (midx - rotatedWidth/2) - Sin * (midy - rotatedHeight/2) + width/2 - w/2)
-        #             y = int(Sin * (midx - rotatedWidth/2) + Cos * (midy - rotatedHeight/2) + height/2 - h/2)
-        #             print("face found in angle", angle)
-        #             location[0] = x
-        #             location[1] = y
-
-        #             faces.append(location)
-                    
-        # -----------------------------------------------
-        # 두명 이상이면 얼굴 큰 사람
-        if len(faces)>1:
-            face_list = []
-            for face in faces:
-                face_list.append(face[2])
-            faces = np.array([faces[np.argmax(np.array(face_list))]])
-
-        if len(faces)==1:
-            if is_detected.value == 0:
-                is_detected.value = 1 
-            [x, y, w, h] = faces[0]
-            face_locations = np.array([[y, x+w, y+h, x]])
-            face_location.array[:] = face_locations[:] # export
-            (top, right, bottom, left) = (y, x+w, y+h, x)
-            # cv2.rectangle(frame, (left, top), (right, bottom), (0,0,255), 2)
-
-        # No face detected
-        else:
-            if is_detected.value == 1:
-                is_detected.value = 0
-            else:
-                pass
-        print(face_locations)
-        # print(is_detected.value)
-
-def face_tracking(face_location, ):
-    np.array([[y, x+w, y+h, x]]) = face_location.array
-    x_pos = x + w/2
-    y_pos = y + h/2 
+        for (top, right, bottom, left) in face_location.array:
+            x = left
+            w = right - left
+            y = top
+            h = bottom - top
+        x_pos = x + w/2
+        y_pos = y + h/2 
     # 모터 제어 파트 추가 
 
 
 ######################################################################################
 
+
 # 얼굴 인식은 is_detected 일때만 돌아가도록 하자
 # 얼굴 인식과 표정 인식을 멀티프로세싱을 돌려 빠르게 처리하도록
 # 인식하고, 인식 횟수가 몇회 이상이면 
 def recognition(frame, face_location_, emotion, is_detected, ):
-    model = model()
-    model.load_weights('model/model.h5')
     # 아는 
     while True:
         if is_detected.value == 1:
             rgb = frame.array.astype(np.uint8)
             face_location = face_location_.array.astype(np.uint8)
-            face_reco = Process(target=face_reco, args=(rgb, face_location, )) # 이 부분이 문제.
-            # 함수 안에서 다른 함수를 호출할 수는 있지만, 멀티프로세싱으로 돌리지는 못하나? 되는데... 일단 다른것부터
-            face_reco.start()
-            face_reco.join()
-            face_emo = Process(target=face_emo, args=(rgb, face_location, model, ))
-            face_emo.start()
-            face_emo.join()
+            
+            loop = asyncio.get_event_loop()
+            loop.run_until_complete(asyncio.gather(
+                face_reco(rgb, face_location),
+                # face_emo(),
+            ))
+
         else:
             time.sleep(0.03)
             pass
 
-def face_reco(rgb, face_location, ):
+
+async def face_reco(rgb, face_location, ):
+    start = time.time()
     with open("face/face_list.json", "r") as f:
         face_list = json.load(f)
     
-    known_face_names = face_list.keys() # list
-    known_face_encodings = np.array(face_list.values()) # numpy.ndarray
-    print(known_face_encodings)
+    known_face_names = list(face_list.keys()) # list
+    known_face_encodings = np.array(list(face_list.values())) # numpy.ndarray
+    # print(known_face_names)
+    # print(known_face_encodings)
     
     ##불러온 파일 이용해서 인코딩 구한다
     face_encoding = face_recognition.face_encodings(rgb, face_location)
+    # print("face_encoding: ",face_encoding[0])
     matches = face_recognition.compare_faces(known_face_encodings, face_encoding[0])
     face_distances = face_recognition.face_distance(known_face_encodings, face_encoding[0])
+    # print("face_distances: ", face_distances)
+    # print("matches: ", matches)
     best_match_index = np.argmin(face_distances)
 
     if matches[best_match_index]:
@@ -148,22 +130,23 @@ def face_reco(rgb, face_location, ):
     else:
         name = "unknown"
     
-    print(name)
-    return name
+    # print(name)
+    # print("face_reco time: ",time.time() - start)
+    # print(" ")
 
-def face_emo(rgb, face_location, model,):
-    gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
+# async def face_emo(rgb, face_location, model,):
+#     gray = cv2.cvtColor(rgb, cv2.COLOR_RGB2GRAY)
     
-    for (top, right, bottom, left) in face_location:
-        roi_gray = gray_for_emotion[top:bottom, left:right]
-        cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
-        prediction = model.predict(cropped_img)
-        # cv2.imwrite('cropped.png', roi_gray)
+#     for (top, right, bottom, left) in face_location:
+#         roi_gray = gray_for_emotion[top:bottom, left:right]
+#         cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (48, 48)), -1), 0)
+#         prediction = model.predict(cropped_img)
+#         # cv2.imwrite('cropped.png', roi_gray)
         
-        if len(prediction) != 0:
-            prediction = prediction[0]
-            prediction = np.rint(prediction/sum(prediction)*100) # %
-            return prediction
+#         if len(prediction) != 0:
+#             prediction = prediction[0]
+#             prediction = np.rint(prediction/sum(prediction)*100) # %
+#             print(prediction)
 
 ##################################################################################
 
@@ -188,30 +171,6 @@ def left_arm(): pass
 def right_arm(): pass
 def doridori(): pass
 
-############################################################################################
-
-def model():    
-    model = Sequential()
-
-    model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1)))
-    model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    model.add(Flatten())
-    model.add(Dense(1024, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(7, activation='softmax'))
-
-    return model
-  
-############################################################################
 
 # 언노운 일정 횟수 이상 들어오면 이 함수 실행하도록
 
@@ -251,10 +210,16 @@ def delete_img():
 
 if __name__ == "__main__":
     try:
+        # model = model()
+        # model.load_weights('model/model.h5')
+
+        HEIGHT = Value('i', 320)
+        WIDTH = Value('i', 480)
+
         save_img()
-        frame = SharedNDArray((480, 640, 3))
+        frame = SharedNDArray((HEIGHT.value, WIDTH.value, 3))
         face_location = SharedNDArray((1,4))
-        print(face_location.array)
+        # print(face_location.array)
         emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
         view_running = Value('i', 1)
@@ -263,13 +228,13 @@ if __name__ == "__main__":
         is_detected = Value('i', 0)
         emotion = Value('i', 4)
 
-        
-        view = Process(target=view, args=(frame, view_running, ))
-        face_tracking = Process(target=face_tracking, args=(frame, face_location, face_tracking_running, is_detected, ))
+        view = Process(target=view, args=(frame, HEIGHT, WIDTH, face_location ,view_running, ))
+        face_tracking = Process(target=face_tracking, args=(face_location, face_tracking_running, ))
         recognition = Process(target=recognition, args=(frame, face_location, emotion, is_detected, ))
 
         view.start()
         face_tracking.start()
+        # face_tracking_move.start()
         recognition.start()
 
         while True:
