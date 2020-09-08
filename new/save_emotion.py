3575d12111a2bb9838b8ece947417b9e5144fa64
Sangwon <born9507@snu.ac.kr>
2020-09-09 05:55:16 +0900

    Add POST connection to server

--- new/save_emotion.py
+++ new/save_emotion.py
@@ -4,6 +4,7 @@ import urllib.request
 import numpy as np
 import json
 from datetime import datetime
+from ftplib import FTP
 
 URL = "http://sangw.iptime.org"
 
@@ -32,45 +33,49 @@ def save_emotion(is_detected, emotion, name_index,known_face_names, ):
     # print(total_emotions)
 
     while True:
-        if time.gmtime(time.time()).tm_sec != check_sec.tm_sec: # 1 
-            if is_detected.value == 1:
-                name = known_face_names[name_index.value]
-                emotion_sum = np.array(total_emotions.get(name))
-                emotion_sum = emotion_sum + emotion.array[:]
-                total_emotions[name] = emotion_sum.tolist()[0]
-                print(total_emotions)
+        try:
+            if time.gmtime(time.time()).tm_sec != check_sec.tm_sec: # 1 
+                if is_detected.value == 1:
+                    name = known_face_names[name_index.value]
+                    emotion_sum = np.array(total_emotions.get(name))
+                    emotion_sum = emotion_sum + emotion.array[:]
+                    total_emotions[name] = emotion_sum.tolist()[0]
+                    # print(total_emotions)
 
-            check_sec = time.gmtime(time.time())
+                check_sec = time.gmtime(time.time())
 
-        if time.gmtime(time.time()).tm_min != check_min.tm_min:
-            now_min = datetime.today().strftime("%Y%m%d%H%M")
-            print(now_min)
-            time_total_emotions[now_min] = total_emotions
-            
-            with open("emotions/emotions.json", "w") as f:
-                json.dump(time_total_emotions, f, indent=2)
+            if time.gmtime(time.time()).tm_min != check_min.tm_min:
+                now_min = datetime.today().strftime("%Y%m%d%H%M")
+                print(now_min)
+                time_total_emotions[now_min] = total_emotions
+                
+                with open("emotions/emotions.json", "w") as f:
+                    json.dump(time_total_emotions, f, indent=2)
 
-            for name in known_face_names:
-                total_emotions[name] = [0,0,0,0,0,0,0]
+                for name in known_face_names:
+                    total_emotions[name] = [0,0,0,0,0,0,0]
+                
+                check_min = time.gmtime(time.time())
+                
             
-            check_min = time.gmtime(time.time())
-            
-        
-        # 5분마다 전송 시도
-        if time.time() - webtime > 5: 
-            # 네트워크 연결 확인    
-            if internet_on() == True:
-                print("Internet Connected")
-                # client = requests.session()
-                # client.get(URL)
-                # csrftoken = client.cookies['csrftoken']
-                # login_data = dict(username=EMAIL, password=PASSWORD, csrfmiddlewaretoken=csrftoken, next='/')
-
-                requests.post(URL, data=json.dumps(time_total_emotions))
-                webtime = time.time()
-            
-            # 전송 실패시 5초 뒤에 다시 시도
-            else:
-                print("No network, Connect to WiFi")
-                webtime = time.time() - 5
+            # 5분마다 전송 시도
+            if time.time() - webtime > 2: 
+                # 네트워크 연결 확인    
+                if internet_on() == True:
+                    print("Internet Connected")
+                    # print(json.dumps(time_total_emotions))
+                    requests.post(URL, data=json.dumps(time_total_emotions))
+                        
+                    webtime = time.time()
+                
+                # 전송 실패시 5초 뒤에 다시 시도
+                else:
+                    print("No network")
+                    webtime = time.time() - 5
+                pass
+        except ConnectionError:
+            pass
+        except KeyboardInterrupt:
+            break
+        except:
             pass
